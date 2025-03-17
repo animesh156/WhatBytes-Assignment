79af59cc9ccae70360ab3494b8ac1b433da6df2b
Animesh  Rathore <animeshrathore255@gmail.com>
2025-03-17 16:47:01 +0530

    Update QuestionAnalysis.js

--- src/components/QuestionAnalysis.js
+++ src/components/QuestionAnalysis.js
@@ -1,6 +1,6 @@
 import RadialProgress from "./RadialProgress";
 
-export default function QuestionAnalysis() {
+export default function QuestionAnalysis({score}) {
   return (
     <div className="border md:h-[335px] h-[300px] mb-3 md:w-[400px]  w-[350px] rounded-md border-gray-300 text-black p-5 gap-y-5  flex flex-col">
       <div className="flex  justify-between">
@@ -10,11 +10,11 @@ export default function QuestionAnalysis() {
       </div>
 
       <p className="text-sm font-medium -mt-1 text-gray-700">
-        <strong>You scored 10 question correct out of 15.</strong> However it
+        <strong>You scored {score} question correct out of 15.</strong> However it
         still <br></br> needs some improvements
       </p>
 
-      <RadialProgress />
+      <RadialProgress score ={score} />
     </div>
   );
 }
